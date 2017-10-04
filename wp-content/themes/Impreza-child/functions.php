0c07aecec04a4fc62ef2cc457ba765390691654d
svirom <svirom@yahoo.com>
2017-10-04 15:12:31 +0300

    first commit

---
+++ wp-content/themes/Impreza-child/functions.php
@@ -0,0 +1,7 @@
+<?php
+/* Custom functions code goes here. */
+
+function my_child_theme_setup() {
+	load_child_theme_textdomain( 'Impreza-child', get_stylesheet_directory() . '/languages' );
+}
+add_action( 'after_setup_theme', 'my_child_theme_setup' );
---
+++ wp-content/themes/Impreza-child/header.php
@@ -0,0 +1,11 @@
+<?php defined( 'ABSPATH' ) OR die( 'This script cannot be accessed directly.' );
+
+/**
+ * The template for displaying pages headers
+ *
+ * Do not overload this file directly. Instead have a look at framework/templates/header.php: you should find all
+ * the needed hooks there.
+ */
+
+//us_load_template( 'templates/header' );
+include get_stylesheet_directory() . '/framework/templates/header.php';
